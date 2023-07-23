d30a8ca42855f576cd3141ee70ebfa585c181292
Tuval Simha <tuval.simha@Gmail.com>
2023-07-23 22:18:01 +0300

    Update src/fets/client.ts
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- src/fets/client.ts
+++ src/fets/client.ts
@@ -1,5 +1,5 @@
 import { type NormalizeOAS, createClient } from "fets";
-import openapi from "./openapi";
+import type openapi from "./openapi";
 
 export const client = createClient<NormalizeOAS<typeof openapi>>({
   endpoint: "https://api.sportsdata.io/v4/soccer/stats",
